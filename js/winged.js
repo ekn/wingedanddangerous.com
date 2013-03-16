e77771fc17023fc40678e918b5cbc23c7901bba8
Elisabeth Nicula <elisabeth@wingedanddangerous.com>
2013-03-16 15:56:26 -0400

    started jekyll, finished footer

--- js/winged.js
+++ js/winged.js
@@ -1,6 +1,6 @@
 maybeShowLogo = function() {
     var w = $(window);
-	$("#nav-logo").toggle((w.scrollTop() >= 158) && (w.width() > 778));
+	$("#nav-logo").toggle((w.scrollTop() >= 158) && (w.width() > 700));
 }
 
 $(window).scroll(maybeShowLogo);
